54ef040af430d5ac4442ce98b7d4a79931e1e280
Jordan Matelsky <j6k4m8@gmail.com>
2023-05-15 15:13:10 -0400

    feat: Add output type arguments

--- grandlite/__init__.py
+++ grandlite/__init__.py
@@ -1,9 +1,25 @@
-import pandas as pd
+import argparse
 import pathlib
 import sys
-from prompt_toolkit import prompt
+
 import networkx as nx
+import pandas as pd
 from grandcypher import GrandCypher
+from prompt_toolkit import prompt
+
+
+def detect_and_load_graph(graph_uri: str) -> nx.Graph:
+    # Detect type of graph
+    graph_path = str(pathlib.Path(graph_uri).absolute().resolve())
+    if graph_path.endswith(".gml") or graph_path.endswith(".gml.gz"):
+        host_graph = nx.read_gml(graph_path)  # type: ignore
+    elif graph_path.endswith(".graphml") or graph_path.endswith(".graphml.gz"):
+        host_graph = nx.read_graphml(graph_path)  # type: ignore
+    elif graph_path.endswith(".gpickle"):
+        host_graph = nx.read_gpickle(graph_path)  # type: ignore
+    else:
+        raise ValueError(f"Unknown graph file type for file '{graph_path}'.")
+    return host_graph
 
 
 def prompt_loop_on_graph(host_graph: nx.Graph):
@@ -33,22 +49,53 @@ def prompt_loop_on_graph(host_graph: nx.Graph):
         print(pd.DataFrame(results))
 
 
-def prompt_loop():
-    args = sys.argv
-    host_graph_name = str(pathlib.Path(args[-1]).resolve())
-    # Detect type of graph
-    if host_graph_name.endswith(".gml") or host_graph_name.endswith(".gml.gz"):
-        host_graph = nx.read_gml(host_graph_name)
-    elif host_graph_name.endswith(".graphml") or host_graph_name.endswith(
-        ".graphml.gz"
-    ):
-        host_graph = nx.read_graphml(host_graph_name)
-    elif host_graph_name.endswith(".gpickle"):
-        host_graph = nx.read_gpickle(host_graph_name)
+def cli():
+    argparser = argparse.ArgumentParser(
+        "An interactive graph query tool for Cypher and other query languages.",
+    )
+    # One positional argument — the filename of the graph to load.
+    argparser.add_argument(
+        "graph",
+        help="The filename of the graph to load.",
+    )
+    # Optional output formats; if none is provided, default to None.
+    argparser.add_argument(
+        "-o",
+        "--output",
+        choices=["csv", "json", "jsonl"],
+        help="The output format to use.",
+        default=None,
+    )
+    # Optional query parameter. If not provided, enters an interactive loop.
+    argparser.add_argument(
+        "-c",
+        "--cypher",
+        help="A Cypher query to run.",
+        default=None,
+    )
+
+    args = argparser.parse_args()
+    host_graph = detect_and_load_graph(args.graph)
+
+    if args.cypher:
+        try:
+            results = GrandCypher(host_graph).run(args.cypher)
+            results = pd.DataFrame(results)
+        except Exception as e:
+            print(e)
+            sys.exit(1)
+        results_formatter = {
+            "csv": lambda x: x.to_csv(sys.stdout),
+            "json": lambda x: x.to_json(sys.stdout, orient="records"),
+            "jsonl": lambda x: x.to_json(sys.stdout, orient="records", lines=True),
+        }
+        if args.output is None:
+            print(results)
+        else:
+            results_formatter[args.output](results)
     else:
-        raise ValueError(f"Unknown graph file type for file '{host_graph_name}'.")
-    prompt_loop_on_graph(host_graph)
+        prompt_loop_on_graph(host_graph)
 
 
 if __name__ == "__main__":
-    prompt_loop()
+    cli()
